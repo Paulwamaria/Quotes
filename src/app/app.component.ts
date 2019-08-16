5b7514975f829a0351342f491cf68d062162b522
Paulwamaria <paulwamaria@gmail.com>
2019-08-16 15:42:04 +0300

    Create a simple array of quotes and display it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  title = 'Favourite Quotes';
+  quotes: string[];
+  constructor() {
+    // tslint:disable-next-line: max-line-length
+    this.quotes = [ 'Luck is preparation meeting the moment of opportunity', 'When they go low, we go high', 'whether you believe you can or you cannot, you are right'];
+  }
 }
---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+// import { Goal } from '../quotes';
+import { Quotes } from '../quotes';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+  // quotes: Quote= [
+
+  // ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
