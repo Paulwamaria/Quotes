da2866287d2ecb24ba70c1af0c5c53d88a5a607d
Paulwamaria <paulwamaria@gmail.com>
2019-08-16 17:28:00 +0300

    Add a quote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Favourite Quotes';
-  quotes: string[];
-  constructor() {
-    // tslint:disable-next-line: max-line-length
-    this.quotes = [ 'Luck is preparation meeting the moment of opportunity', 'When they go low, we go high', 'whether you believe you can or you cannot, you are right'];
-  }
+  // quotes: string[];
+  // constructor() {
+  //   // tslint:disable-next-line: max-line-length
+  //   this.quotes = [ 'Luck is preparation meeting the moment of opportunity', 'When they go low, we go high', 'whether you believe you can or you cannot, you are right'];
+  // }
 }
